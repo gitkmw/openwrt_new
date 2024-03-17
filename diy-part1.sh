1baca896e3e998a1332084e5c06f577904a51c3f
gitkmw <309209843@qq.com>
2024-03-17 13:23:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,6 +20,7 @@
 # add luci-plugins
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+rm -rf feeds/packages/net/{adguardhome}
 
 # luci-theme-argon for 18.06 by jerrykuku
 rm -rf luci-theme-argon
